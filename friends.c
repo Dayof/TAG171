c652ebea124a6aa5ef340d17323f3b394d53b949
Day <dayannefernandesc@gmail.com>
2017-03-23 11:45:07 -0300

    process registration and names into graph architecture

--- friends.c
+++ friends.c
@@ -1,21 +1,48 @@
 #include "friends.h"
 
-// void addInVertices()
-// {
-// }
+void initList(int i)
+{
+  ALL_VERTEX[i].first = (EDGE*)malloc(sizeof(EDGE));
+  ALL_VERTEX[i].last = ALL_VERTEX[0].first;
+}
+
+void insertVertex(int i, char *reg, char *name, char *links)
+{
+  VERTEX *new_vertex = (VERTEX*)malloc(sizeof(VERTEX));
+  strcpy(new_vertex->reg, reg);
+  strcpy(new_vertex->name, name);
+
+  EDGE *new_edge = (EDGE*)malloc(sizeof(EDGE));
+  new_edge->data = *new_vertex;
+  new_edge->next = NULL;
+
+  initList(i);
+  ALL_VERTEX[i].last->next = &new_edge->data;
+  ALL_VERTEX[i].last = new_edge;
+  // printf("%s - %s\n", ALL_VERTEX[i].last->data.reg, ALL_VERTEX[i].last->data.name);
+}
 
 // void processLinks(char *links)
 // {
 // }
 
+// void findError(char *links)
+// {
+// }
+
 int main(){
   FILE *pF = fopen("amigos.txt", "r");
-  char mat[15], nome[50], links[50];
+  char reg[15], name[50], links[50];
+  int count_vertex = 0;
 
   if(pF != NULL)
   {
-    while (fscanf(pF, "%s | %[^|] | %[^\n]", mat, nome, links) != EOF)
-    printf("%s - %s - %s\n", mat, nome, links);
+    while (fscanf(pF, "%s | %[^|] | %[^\n]", reg, name, links) != EOF)
+    {
+      insertVertex(count_vertex, reg, name, links);
+      ++count_vertex;
+    }
+
     fclose(pF);
   }
 
--- friends.h
+++ friends.h
@@ -1,6 +1,19 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_VERTEX 39
 
-typedef struct{
-  char mat[15];
+typedef struct vertex{
+  char reg[15];
   char name[50];
-}VERTICE;
+}VERTEX;
+
+typedef struct edge{
+  VERTEX data, *next;
+}EDGE;
+
+typedef struct list{
+  EDGE *first, *last;
+}LIST;
+
+LIST ALL_VERTEX[MAX_VERTEX];
